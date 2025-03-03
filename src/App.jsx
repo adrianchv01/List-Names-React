6f03920d08bbddca4e7f4515bfa6cabbf9ff159d
adrianchv01 <adrianchv04@gmail.com>
2025-03-02 23:38:13 -0500

    list id

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,42 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { id: 1,
+      name: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
 
   const handleNewName = e => setNewName(e.target.value);
 
+  const addNewName = (event) =>{
+    event.preventDefault();
+    const nameObject = {
+      id: persons.length + 1,
+      name: newName
+  
+    }
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  }
+  
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addNewName}>
         <div>
-          name: <input onChange={handleNewName}/>
+          name: <input value={newName} onChange={handleNewName}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {persons.map((per) => 
+          <li key={per.id}>{per.name}</li>
+        )}
+      </ul>
       <div>debug: {newName}</div>
     </div>
   )
